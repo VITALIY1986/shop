f6b1e3050cba4e1b610d1a115e5383ba19cbd0ef
VITALIY1986 <staslego1986@gmail.com>
2020-06-05 00:27:34 +0300

    first commit

---
+++ src/components/CartItemList/index.js
@@ -0,0 +1,90 @@
+/* eslint-disable camelcase */
+import React from 'react'
+import {Link} from 'gatsby'
+import {Item, Button, Loader, Message, Responsive} from 'semantic-ui-react'
+
+export default ({items, removeFromCart, loading, completed}) => {
+  if (loading) return <Loader active inline="centered" />
+
+  if (completed)
+    return (
+      <Message success>
+        <Message.Header>Your placed!</Message.Header>
+        <p>Congratulations. Your order and payment has been accepted.</p>
+      </Message>
+    )
+
+  if (items.length === 0)
+    return (
+      <Message warning>
+        <Message.Header>Your cart is empty</Message.Header>
+        <p>
+          You will need to add some items to the cart before you can checkout.
+        </p>
+      </Message>
+    )
+  const mapCartItemsToItems = items =>
+    items.map(({id, product_id, name, quantity, meta, image}) => {
+      const price = meta.display_price.with_tax.unit.formatted || ''
+      const imageUrl = image.href || '/static/moltin-light-hex.svg'
+
+      const DesktopItemImage = () => (
+        <Item.Image
+          src={imageUrl}
+          alt={name}
+          size="small"
+          style={{background: '#f2f2f2'}}
+        />
+      )
+      const MobileItemImage = () => (
+        <Item.Image
+          src={imageUrl}
+          alt={name}
+          size="small"
+          style={{background: 'none'}}
+        />
+      )
+
+      return {
+        childKey: id,
+        header: (
+          <Item.Header>
+             <form name="Contact Form" method="POST" data-netlify="true">
+      <input type={quantity} name={quantity} value={price} />
+      <div>
+        <label>Your Email:</label>
+        <input type={name} name={name} />
+      </div>
+      <div>
+        <label>Message:</label>
+        <textarea name="message" />
+      </div>
+      <button type="submit">Send</button>
+    </form>
+            <Link to={`/product/${product_id}/`}>{name}</Link>
+          </Item.Header>
+        ),
+        image: (
+          <React.Fragment>
+            <Responsive as={MobileItemImage} {...Responsive.onlyMobile} />
+            <Responsive
+              as={DesktopItemImage}
+              minWidth={Responsive.onlyTablet.minWidth}
+            />
+          </React.Fragment>
+        ),
+        meta: `${quantity}x ${price}`,
+        description: 'asfasfasfasafasfa.',
+        extra: (
+          <Button
+            basic
+            icon="remove"
+            floated="right"
+            onClick={() => removeFromCart(id)}
+          />
+         
+        ),
+      }
+    })
+  return <Item.Group divided items={mapCartItemsToItems(items)} />
+}
